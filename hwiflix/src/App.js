443d6df109a9965d3f70ab155c647343bc34700d
Alisser <gnlrjswjd@naver.com>
2024-10-08 13:11:01 +0900

    useEffect

--- hwiflix/src/App.js
+++ hwiflix/src/App.js
@@ -1,10 +1,29 @@
-import Button from "./Button";
-import styles from "./Button.module.css";
+import { useState, useEffect } from "react";
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+
+  useEffect(() => {
+    console.log("Call the API");
+  }, []); //처음 한번만 렌더할 함수는 이곳에 넣어준다
+
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 6) {
+      console.log("SERACH", keyword);
+    }
+  }, [keyword]); // keyword가 바뀔때만 실행
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text="Continue" />
+      <input 
+      value={keyword}
+      onChange={onChange} 
+      type="text" 
+      placeholder="Serch"/>
+      <h1>{counter}</h1>
+      <button onClick={onClick}>나를 눌러요</button>
     </div>
   );
 }
